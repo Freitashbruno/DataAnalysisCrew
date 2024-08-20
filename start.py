e29cc2c9730775d03b36a819b06671a229efc821
Bruno Borges <freitashbruno@gmail.com>
2024-08-20 10:42:50 -0300

    Update start.py

--- start.py
+++ start.py
@@ -1,21 +1,21 @@
 import sys
 import os
 
-# Adicione o caminho onde o utils.py está localizado
+# Adiciona o caminho onde o utils.py está localizado
 sys.path.append('/content/drive/MyDrive/DataAnalysisCrew')
 
-# Agora, você pode importar a função
+# Importa a função get_openai_api_key do utils.py
 from utils import get_openai_api_key
 
-# Utilize a função para obter a chave da API
+# Obtém a chave da API OpenAI e define a variável de ambiente
 openai_api_key = get_openai_api_key()
-os.environ["OPENAI_API_KEY"] = openai_api_key  # Certifique-se de que a chave é atribuída aqui
+os.environ["OPENAI_API_KEY"] = openai_api_key  # Certifique-se de que a chave é atribuída corretamente aqui
 os.environ["OPENAI_MODEL_NAME"] = 'gpt-3.5-turbo'
 
-# Configure o PYTHONPATH
+# Configura o PYTHONPATH para incluir o diretório src
 sys.path.insert(0, os.path.abspath('src'))
 
-# Execute o script principal
+# Importa e executa o script principal
 from data_analysis_crew.main import main
 
 if __name__ == "__main__":
